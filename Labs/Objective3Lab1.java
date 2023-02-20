5fc1639eb177e4f13f48d6833d98121d0fea2c94
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-02-19 21:37:36 -0700

    Objective3Lab1 completed

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,9 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		int num1 = 25;
+		int num2 = 42;
+		int num3 = 14;
+		int average = (num1 + num2 +num3)/3;
+		System.out.println(average);
+	}
+}

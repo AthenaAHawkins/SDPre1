f1022f0b9e1b4b48d3bf6d6f2e46b45d4fc2b9bb
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-02-19 20:06:07 -0700

    Objective2Lab3 complete

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int year = 1993;
+		String monthBorn = "December";
+		boolean wknd = true;
+		System.out.println(year);
+		System.out.println(monthBorn);
+		System.out.println(wknd);
+
+	}
+}

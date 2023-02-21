2f9668760ece98beb944b410c73a8a8622cdf97f
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-02-21 12:14:41 -0700

    Objective3Lab2 completed

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+	public static void main(String[] args) {
+		double side1, side2, hypotenuse;
+		side1 = 11.4;
+		side2 = 6.5;
+		hypotenuse = Math.sqrt((side1*side1) + (side2*side2));
+		System.out.println("The hypotenuse of a triangle with sides 11.4 and 6.5 is" + " " + hypotenuse);
+	}
+}

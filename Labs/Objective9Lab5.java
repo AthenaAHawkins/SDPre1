8e88f28c1109161566051715adc56edd1a7490d1
Athena Hawkins <Athena.a.hawkins@gmail.coml>
2023-03-02 23:10:22 -0700

    Objective9Lab5 working

---
+++ Labs/Objective9Lab5.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class methodsLab5 {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner (System.in);
+
+					double num1, num2;
+					boolean KeepGoing = true;
+					int choice;
+					double answer = 0.0;
+
+		System.out.println("Please give me a number");
+		num1 = kb.nextDouble();
+		System.out.println("Please give me another number");
+		num2 = kb.nextDouble();
+
+					while (keepGoing) {
+						printMenu();
+						System.out.print("Which would you like to do? ");
+						choice = kb.nextInt();
+
+						switch (choice) {
+
+						case 1:
+							System.out.println(num1 + "+" + num2  + "=" + sum);
+
+						case 2: 
+							System.out.println("The average of " + num1 + " and " + num2 + " is: " + average);
+
+						case 3: 
+							System.out.println("The amount in tax to be collected from a purchase of " + num1 + " and " + num2 + " is: " + calcTax);
+
+						case 4: 
+							System.out.println("You've chosen to quit");
+							break;
+
+						default:
+							System.out.println("Invalid Entry. Please try again.");
+						}
+
+					}
+					kb.close();
+
+
+	public static void printMenu() {
+		System.out.println();
+		System.out.println("========== MENU =========");
+		System.out.println("|                       |");
+		System.out.println("|    1. Add Numbers     |");
+		System.out.println("|    2. Find Average    |");
+		System.out.println("|    3. Calculate Tax   |");
+		System.out.println("|    4. Exit            |");
+		System.out.println("|                       |");
+		System.out.println("=========================");
+		System.out.println();
+					}
+
+	public static double findSum(double x, double y) {
+		double sum = x + y;
+		}						
+
+
+	public static double findAverage(double x, double y) {
+		double average = ( x + y) / 2;
+		return average;
+	}
+
+	public static double calcTax(double x, double y) {
+		double calcTax = ((x + y) * (0.831)); 
+		return calcTax;
+	}
+}
+
+}	
+						
+
